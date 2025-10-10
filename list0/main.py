d1b8d0723d9156b605d1cbb3ed34407cb05d5099
Jan Pultorak <janpultorak02@proton.me>
2025-10-10 10:27:44 +0200

    Add ruff with pre-commit-hooks

--- list0/main.py
+++ list0/main.py
@@ -2,8 +2,9 @@ import tsplib95
 
 
 def read_example():
-  dantzing42 = tsplib95.load('../datasets/dantzig42.tsp')
-  print(dantzing42.as_name_dict())
+    att48 = tsplib95.load("../datasets/att48.opt.tour")
+    print(att48.as_name_dict())
+
 
 if __name__ == "__main__":
-  read_example()
+    read_example()
