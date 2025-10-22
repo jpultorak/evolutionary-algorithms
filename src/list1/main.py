7c37d98dd5c4092c201cead87ea8a2dbbeed38f5
Jan Pultorak <janpultorak02@proton.me>
2025-10-22 15:58:46 +0200

    Add deceptive one max

--- src/list1/main.py
+++ src/list1/main.py
@@ -2,20 +2,44 @@ import numpy as np
 
 from list1.algorithms.pbil import pbil
 
-INSTANCE_LENGTH = 100
+INSTANCE_LENGTH = 10
 
 
 def f_one_max(x: np.ndarray):
+    return np.sum(x)
+
+
+def f_deceptive_one_max(x: np.ndarray):
     d = len(x)
-    goal = np.ones(d, dtype=np.bool)
-    return np.sum(x == goal)
+    ones_count = np.sum(x)
+
+    if ones_count == 0:
+        return d + 1
+    return ones_count
+
+
+def f_k_deceptive_one_max(x: np.ndarray, k):
+    d = len(x)
+    res = 0
+    for i in range(0, d, k):
+        r = min(d, i + k)
+        total = r - i
+        sub_x = x[i:r]
+
+        ones_count = np.sum(sub_x == 1)
+        if ones_count == 0:
+            res += total + 1
+        else:
+            res += ones_count
+
+    return res
 
 
 def main():
     prob_vector = pbil(
         d=INSTANCE_LENGTH,
-        eval_f=f_one_max,
-        pop_size=100,
+        eval_f=f_deceptive_one_max,
+        pop_size=3,
         learn_rate=0.1,
         mut_prob=0.02,
         mut_shift=0.05,
