7b707af1fec5087fa31a44d607fb7a2379ebad3e
Jan Pultorak <janpultorak02@proton.me>
2025-10-29 14:23:02 +0100

    Add jupyter notebook

--- src/list1/main.py
+++ src/list1/main.py
@@ -1,15 +1,16 @@
 import numpy as np
 
 from list1.algorithms.pbil import pbil
+from list1.utils import random_population
 
-DECEPTIVE_K = 3
+DECEPTIVE_K = 8
 
-INSTANCE_LENGTH = 10
-POP_SIZE = 10
+INSTANCE_LENGTH = 50
+POP_SIZE = 100
 LEARN_RATE = 0.1
 MUT_PROB = 0.02
 MUT_SHIFT = 0.05
-MAX_ITER = 200
+MAX_ITER = 400
 
 rng = np.random.default_rng()
 
@@ -94,13 +95,27 @@ def k_deceptive_one_max():
         rng=rng,
     )
 
-    print(prob_vector)
+    opt = opt_k_deceptive(INSTANCE_LENGTH, k=DECEPTIVE_K)
+    print(f"OPTIMAL: {opt}")
+    pop = random_population(prob_vector, 100, rng)
+    rank = np.array([f_k_deceptive_one_max(x) for x in pop])
+    best = rank.max()
+    print(best)
+    print_prob_vector(prob_vector)
     print()
 
 
+def print_prob_vector(p, decimals=5):
+    p = np.asarray(p, float)
+    np.set_printoptions(suppress=True)
+    for i in range(0, len(p), 10):
+        chunk = p[i : i + 10]
+        print(" ".join(f"{x:.{decimals}f}" for x in chunk))
+
+
 def main():
-    one_max()
-    deceptive_one_max()
+    # one_max()
+    # deceptive_one_max()
     k_deceptive_one_max()
 
 
