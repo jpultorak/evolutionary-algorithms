e65fa052f21b13e980e9fc9e1727de2e43651b37
Jan Pultorak <janpultorak02@proton.me>
2026-01-26 00:33:09 +0100

    Project init

---
+++ src/project/gymnasium-test.py
@@ -0,0 +1,21 @@
+import gymnasium as gym
+
+env = gym.make("CartPole-v1", render_mode="human")
+
+observation, info = env.reset()
+print(f"Starting observation: {observation}")
+
+episode_over = False
+total_reward = 0
+
+while not episode_over:
+    pole_angle = observation[2]
+    action = 1 if pole_angle > 0 else 0
+
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    total_reward += reward
+    episode_over = terminated or truncated
+
+print(f"Episode finished! Total reward: {total_reward}")
+env.close()
